bd17c32d47cf3bd4469dffbc6a722bb5fad559c9
Alex <2022alex.skrin@gmail.com>
2025-05-31 17:06:42 +0200

    PlaySound and micro record

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import socket
+
+serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+port =12345 
+
+print(socket.gethostname())
+serversocket.bind((socket.gethostname(),port))
+serversocket.listen(5)
+
+
+while True:
+    (clientsocket,addres) = serversocket.accept()
+    print("Got connection from ", addres)
+    clientsocket.send("Hello World\n".encode())
+    clientsocket.close()
+
+serversocket.close()
